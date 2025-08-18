7eefa2a1b79651b03c5a48ca124267c8b2a1e30d
rodri <rodrihercar24@gmail.com>
2025-08-17 20:33:02 -0600

    Termine de escribir las preguntas de eleccion multiple

--- data_base.py
+++ data_base.py
@@ -1,3 +1,17 @@
-print("AQUÍ VAN LAS PREGUNTAS")
+from questions import *
 
 
+questions_list = [
+    MultipleChoiceQuestion("¿Quién compuso la canción Luna de Xelajú?",
+                           ["Miguel Angel Asturias", "Paco Pérez", "Ricardo Arjona"], "Paco Pérez"),
+    MultipleChoiceQuestion("¿Cuál es el tercer planeta del sistema solar?", ["Venus", "Tierra", "Marte"],
+                           "Tierra"),
+    MultipleChoiceQuestion("¿Qué se celebra el 15 de septiembre en Guatemala?", ["Quema del diablo", "Semana Santa", "Independencia"],
+                           "Independencia"),
+    MultipleChoiceQuestion("¿De quién es la canción Almohada?", ["José José", "Juan Gabriel", "Luis Miguel"],
+                           "José José"),
+    MultipleChoiceQuestion("¿Cuál es el libro sagrado de los Mayas?", ["Popol Vuh", "Kik'aslemaal", "Ri tzij kek'iyik"],
+                           "Popol Vuh"),
+
+
+]
