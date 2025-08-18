1d09a6523f0c95443873a727ca504b693f3ef57d
rodri <rodrihercar24@gmail.com>
2025-08-17 18:20:46 -0600

    clase pregunta

--- questions.py
+++ questions.py
@@ -1,5 +1,5 @@
 
 class Question:
-    def __init__(self, question, answer):
-        self.question = question
-        self.answer = answer
+    def __init__(self):
+        self.question = str
+        self.answer = None
