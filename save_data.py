7c86416e9255e964751b88f812dd83bafcaab51f
Rpinto30 <rodrigopinto3007@gmail.com>
2025-08-17 23:27:09 -0600

    Creación de funciones para guardar y cargar datos, con ayuda de la libreria json se crean archivos json donde se guardaran los puntajes, se crea automatico cuando no exista

--- save_data.py
+++ save_data.py
@@ -1,1 +1,14 @@
-print("AQUÍ SE ADMINISTRARÁ EL GUARDADO Y CARGADO DEL JUEGO")
+import json
+
+def save_data(points:dict):
+    with open("data.json", mode="w", encoding="utf-8") as data_file:
+        json.dump(points, data_file)
+
+
+def load_data():
+    try:
+        with open("data.json", mode="r", encoding="utf-8") as read_file:
+            return json.load(read_file)
+    except FileNotFoundError: save_data({})
+
+load_data()
