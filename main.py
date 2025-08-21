0a4efe1d60a2d0dd0625878800403d7d61f7931d
Aileen127 <fleuraileen394@gmail.com>
2025-08-21 11:40:06 -0600

    Comienzo de menú y un poco del códdigo de trivia

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 print("AQUÍ VA EL MENÚ DONDE EL USUARIO INTERACTUARÁ")
 
-# Prueba de
+# Ingreso del menú
+
+from trivia_manager import User, Trivia, Game
+from presenter import ado_robot, error_bot
+
+while True:
+    ado_robot()
+    print("\n --- ¡Bienvenido al juego de Trivia! ---")
+    print("1. Ingresar al juego")
+    print("2. Visualizar puntajes y eliminar usuario")
+    print("3. Salir")
+
+    option1 = int(input("Ingresa la opcion de tu preferencia (1 - 2): "))
+    # Inicio de match (ejecucion de opciones)
+    match option1:
+        case 1:
+            print("\n --- Frase de juego ---")
+            print("1. Jugar de nuevo")
 
 
 
--- trivia_manager.py
+++ trivia_manager.py
@@ -1,1 +1,33 @@
 print("Aquí ira la clase que administre la trivia")
+from data_base import questions_list
+
+
+class Trivia:
+    def __init__(self, game, user):
+        counter = 0
+        self.user = user
+        self.game = game
+        self.users_game = {}
+
+class Game:
+    def __init__(self, game, score):
+
+class User:
+    def __init__(self, user):
+        self.users = []
+
+    def add_user(self):
+        try:
+            user = input("Ingresa el nombre del usuario").lower()
+            if user in self.users:
+                print("El usuario ya existe dentro del juego.")
+            else:
+                self.users.append(user)
+
+        except Exception as e:
+            print("Error detectado", e)
+
+    def existent_user(self, user):
+        user = input("Ingresa el nombre del usuario al que deseas igresar: ").lower()
+        if user in self.users:
+
