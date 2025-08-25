3ea3fbfe0373674ee7643d5c129b70a95b1f5a24
Aileen127 <fleuraileen394@gmail.com>
2025-08-24 19:04:19 -0600

    Cambio de clase Game, mejora del metodo start game y de clase User el metodo add user

--- trivia_manager.py
+++ trivia_manager.py
@@ -5,22 +5,16 @@ from manager_file import GameManager
 import  random
 class User:
     def __init__(self):
-        self.users = [] # Lista que guarda usuarios
+        self.user_name = "" # Lista que guarda usuarios
 
-    def add_user(self):
+    def add_user(self, dict_user):
         user = input("Ingresa el nombre del usuario").lower()
-        if user in self.users:
-                print("El usuario ya existe dentro del juego.")
+        if user in dict_user:
+            print(f"Volviendo a jugar como {user}.")
         else:
-                self.users.append(user)
+            print("¡Se ha generado una nueva partida!")
+            dict_user[user] = 0 # Nueva partida
 
-    def show_users(self):
-        print("\n Usuarios en el juego:")
-        if self.users:
-            for user in self.users:
-                print(f"- {user}")
-        else:
-            print("No hay usuarios registrados")
 
     def delete_user(self, user_deleted):
         user_deleted = input("Ingresa el nombre de usuario que deseas eliminar: ").lower()
@@ -30,31 +24,28 @@ class User:
             print("EL usuario no se encuentra registrado en el juego")
 
 class Game(User):
-    def __init__(self):
+    def __init__(self, usuario):
         super().__init__()
-        self.points_list = {}
+        self.user = user
 
     def new_game(self, user):
         score = 0
         if user in self.users:
             for user in self.users:
-                self.users_game[user] = { user : score}
+                # self.points_list[user] = { user : score} # Error del point list de diccionario
+                pass
             print("¡Se ha iniciado un nuevo juego!")
         else:
             print("No se encuentran usuarios registrados.")
 
-    def start_game(self, user):
-
-        user = input("Ingresa el usuario para iniciar el juego: ").lower()
-        if user in self.users:
-            print("\n Has iniciado el juego!")
-            question = random.sample(questions_list, k = 10 )
-            for q in range(len(question)):
-                print("Pregunta")
-                print(question[q].question)
-                respuesta = input("Ingresa la respuesta: ").lower()
-                if respuesta == question[q].correct_answer.lower():
-                    print("La respuesta es correcta")
+    def start_game(self, point):
+        print("\n Has iniciado el juego!")
+        question = random.sample(questions_list, k=10)
+        for q in question:
+            print("Pregunta:")
+            answer = q.show_question() # Muestra y Guarda la respuesta
+            q.verify_answer(answer, )
+            print("La respuesta es correcta")
 
 
 
